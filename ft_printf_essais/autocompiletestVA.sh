4fe78e5e64d25441b083c40bd2ecd2e00df0f239
Stephane Vogrig <svogrig@3C2.42angouleme.fr>
2023-11-23 03:37:36 +0100

    essais des variadiques

---
+++ ft_printf_essais/autocompiletestVA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+clear
+
+cc test_VA_list.c -o test_VA
+
+./test_VA
+sleep 5
+done
--- ft_printf_essais/test_VA_list.c
+++ ft_printf_essais/test_VA_list.c
@@ -6,11 +6,66 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 20:40:30 by svogrig           #+#    #+#             */
-/*   Updated: 2023/11/21 21:00:14 by svogrig          ###   ########.fr       */
+/*   Updated: 2023/11/22 02:21:17 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int min(void)
+#include <stdarg.h>
+#include <stdio.h>
+
+void	foochar(va_list args)
+{
+	printf("\'%c\' | ", va_arg(args, char));
+}
+
+void	foostr(va_list args)
+{
+	printf("\"%s\" | ", va_arg(args, char*));
+}
+
+void	fooint(va_list args)
+{
+	printf("%d | ", va_arg(args, int));
+}
+
+void	foofoo(char *types, ...)
+{
+	va_list	args;
+
+	printf("essai foofoo : | ");
+	va_start(args, types);
+	while(*types)
+	{
+		if(*types == 'i')
+			fooint(args);
+		else if(*types == 's')
+			foostr(args);
+		else if(*types == 'c')
+			foochar(args);
+		types++;
+	}
+	printf("\n");
+
+	va_end(args);	
+}
+
+void foo(char *types, ...)
+{
+	va_list	args;
+
+	printf("essai foo : | ");
+	va_start(args, types);
+	while(*types)
+	{
+		printf("%d | ", va_arg(args, int));
+		types++;
+	}
+	va_end(args);
+	printf("\n");
+}
+int main(void)
 {
 	foo("iii", 1, 2, 3);
+	foofoo("isc", 1, "bonjour", 'A');
+	return (0);
 }

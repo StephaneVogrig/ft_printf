4c1d5376ec454ddb52eaba268fdec59947f4bf24
StephaneVogrig <stephane.vogrig@gmail.com>
2024-01-10 00:15:22 +0100

    correction des typedef signed

--- ft_printf/vs_type.h
+++ ft_printf/vs_type.h
@@ -6,17 +6,17 @@
 /*   By: stephane <stephane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 23:02:22 by stephane          #+#    #+#             */
-/*   Updated: 2024/01/09 17:49:12 by stephane         ###   ########.fr       */
+/*   Updated: 2024/01/10 00:13:07 by stephane         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #ifndef VS_TYPE_H
 # define VS_TYPE_H
 
-typedef long int			t_int8;
-typedef long int			t_int16;
-typedef long int			t_int32;
-typedef long int			t_int64;
+typedef signed char			t_int8;
+typedef signed short		t_int16;
+typedef signed int			t_int32;
+typedef signed long int		t_int64;
 
 typedef unsigned char		t_ui8;
 typedef unsigned short		t_ui16;

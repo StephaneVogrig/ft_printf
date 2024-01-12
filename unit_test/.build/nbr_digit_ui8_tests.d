26534afcdf49d6d1a19e0862436708200e8fdece
Stephane Vogrig <svogrig@3B4.42angouleme.fr>
2024-01-12 18:01:37 +0100

    nettoyage .o et .d

--- unit_test/.build/nbr_digit_ui8_tests.d
+++
@@ -1,6 +0,0 @@
-.build/nbr_digit_ui8_tests.o: nbr_digit_ui8_tests.c nbr_digit_unit_test.h \
- unit_test.h nbr_digit_ui.h vs_type.h
-nbr_digit_unit_test.h:
-unit_test.h:
-nbr_digit_ui.h:
-vs_type.h:
--- unit_test/.build/unit_test.d
+++
@@ -1,6 +0,0 @@
-.build/unit_test.o: unit_test.c unit_test.h nbr_digit_unit_test.h \
- nbr_digit_ui.h vs_type.h
-unit_test.h:
-nbr_digit_unit_test.h:
-nbr_digit_ui.h:
-vs_type.h:
